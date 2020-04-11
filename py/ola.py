848ac397bfdd0487e5dc5dac78d54aa19b213cf5
Magia19 <eunada19@gmail.com>
2020-04-11 17:53:16 +0100

    java

--- py/ola.py
+++ py/ola.py
@@ -1,7 +1,12 @@
 import sys
 print( "Sistema %d " % sys.version)
 a = [5,10,3]
+for i in ranger(a):
+    {
+print("Digite O Indice:")
+a[i]
 try:
-    print(a[3])
+    a[3]
 except IndexError:
     print("O Indice Inserido Nao Exite , Obrg")
+}
