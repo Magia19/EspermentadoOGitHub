bc23616a4d6169e40d1107335308ef67c66e0bea
Magia19 <eunada19@gmail.com>
2020-04-11 17:59:09 +0100

    java1

---
+++ java/ola.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ola {
+  public static void main(String[] args) {
+    Scanner Dado = new Scanner(System.in);
+    System.out.println("Digite O Nome :");
+    String Nome = Dado.nextLine();
+    System.out.println("Ola! ,"+Nome+" Seja Bem Vindo");
+  }
+}
